b11d357e695e73669a475c4d833f347fa3c3c29d
Lucas Henrique <lukdom8@gmail.com>
2024-01-25 18:49:00 -0300

    primeiro commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+const Search = ({ search, setSearch }) => {
+  return <div className="Search">
+    <h2>Pesquisa</h2>
+    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar..."/>
+  </div>
+}
+
+export default Search
